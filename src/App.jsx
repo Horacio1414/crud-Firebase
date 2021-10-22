f5b8f1a1cb6d9192434130d45f6f75baef80d971
Horacio1414 <horaciodemaria@gmail.com>
2021-10-22 17:28:23 -0300

    agregarTarea

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {firebase} from "./firebase"
 function App() {
 
   const [tareas,setTareas] = React.useState([])
+  const [tarea,setTarea] = React.useState("")
 
 
   React.useEffect(() => {
@@ -29,19 +30,70 @@ function App() {
     obtenerDatos()
   }, [])
 
+  const agregar = async (e)=>{
+    e.preventDefault()
+    if(!tarea.trim()){
+      console.log("vacio")
+      return
+     
+    }
+
+    try{
+
+      const db = firebase.firestore()
+      const nuevaTarea = {
+        name:tarea,
+        fecha:Date.now()
+      }
+
+      const data = await db.collection("tareas").add(nuevaTarea)
+      setTarea("")
+
+    }catch(error){
+      console.log(error)
+    }
+
+
+  }
+
 
 
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-6">
-          Listar las tareas
+         <ul className="list-group">
+
+           {tareas.map((item)=>{
+             return (
+               <li className="list-group-item" key={item.id}>
+                 {item.name}
+
+               </li>
+             )
+           })}
+         </ul>
         </div>
         <div className="col-md-6">
-          Formulario
+          <h3>Formulario</h3>
+          <form onSubmit={agregar}>
+            <input 
+            type="text" 
+            placeholder="ingresar tarea"
+            className="form-control mb-2"
+            onChange = {(e)=>{
+              return setTarea(e.target.value)
+            }}
+            value = {tarea}
+            
+            />
+            <button className="btn btn-dark btn block"
+            type ="submit"
+            >Apretame</button>
+          </form>
         </div>
       </div>
-      jskdjskdjsk
+    
     </div>
   );
 }
